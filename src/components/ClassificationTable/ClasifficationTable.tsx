f233a1ebc5219b8c5441874cfbd9b08b1b58f524
Cesar Cardozo <cesar.cardozo@blackboard.com>
2023-01-26 18:13:11 -0500

    Version inicial orden salida

--- src/components/ClassificationTable/ClasifficationTable.tsx
+++ src/components/ClassificationTable/ClasifficationTable.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
-import { RacersGeneral } from '../../data/mockData';
-import ClasifficationTableItem from './ClasifficationTableItem';
+import { RacersGeneralPartial } from '../../data/mockData';
+import './ClasifficationTable.scss';
+
+export interface IClasifficationTableItemProps {
+  id: string;
+  name?: string;
+  nick: string;
+  points: number;
+  previousTime?: string;
+  improvement?: number;
+}
+
+function ClasifficationTableItem(props: IClasifficationTableItemProps) {
+  const { id, name, nick, points,previousTime, improvement } = props;
+  return (
+    <tr className='table-item'>
+      <td>{id}</td>
+      <td>{nick}</td>
+      <td>{points}</td>
+      {previousTime !== undefined && <td>{previousTime}</td>}
+      {improvement !== undefined && <td>{improvement}</td>}
+    </tr>
+  );
+}
 
 function ClasifficationTable() {
   return (
     <table>
-      <tr>
-        <th>Nombre</th>
-        <th>Puntos</th>
-        <th>Mejora</th>
-      </tr>
-      {RacersGeneral.map((racer) => {
-        return <ClasifficationTableItem {...racer} />;
-      })}
+      <thead>
+        <tr>
+          <th colSpan={4} className='race-tittle'>
+            <a href='https://strava.app.link/AUAAw4FFSwb' target='_blank'>
+              <h2>Segmento Alto Sote (1)</h2>
+            </a>
+            <span>Orden de salida</span>
+          </th>
+        </tr>
+        <tr className='header'>
+          <th>#</th>
+          <th>Nombre</th>
+          <th>Puntos</th>
+          <th>Tiempo</th>
+          {/* <th>% Mejora</th> */}
+        </tr>
+      </thead>
+      <tbody>
+              {RacersGeneralPartial.map((racer) => {
+          return <ClasifficationTableItem {...racer} />;
+        })}
+      </tbody>
     </table>
   );
 }
--- src/data/mockData.tsx
+++ src/data/mockData.tsx
@@ -1,4 +1,5 @@
-import {IClasifficationTableItemProps} from '../components/ClassificationTable/ClasifficationTableItem';
+import { IClasifficationTableItemProps } from "../components/ClassificationTable/ClasifficationTable";
+
 
 export const RacersGeneral: IClasifficationTableItemProps[] =  [
   { id:'1', nick: 'Aleja', points: 0, improvement: 0 },
@@ -15,5 +16,28 @@ export const RacersGeneral: IClasifficationTableItemProps[] =  [
   { id:'12', nick: 'Carlos', points: 0, improvement: 0 },
   { id:'13', nick: 'Toño', points: 0, improvement: 0 },
   { id:'14', nick: 'Jonas', points: 0, improvement: 0 },
+];
+
+export const RacersGeneralPartial: IClasifficationTableItemProps[] =  [
+  { id:'1', nick: 'Fabio', points: 0, previousTime: '34:32' },
+  { id:'2', nick: 'Aleja', points: 0, previousTime: '27:11' },
+  { id:'3', nick: 'rodrigo', points: 0, previousTime: '28:18' },
+  { id:'4', nick: 'Hernan', points: 0, previousTime: '25:58' },
+  { id:'5', nick: 'Laura', points: 0, previousTime: '24:48' },
+  { id:'6', nick: 'Mariana', points: 0, previousTime: '24:15' },
+  { id:'7', nick: 'Mechas', points: 0, previousTime: '23:57' },
+  { id:'8', nick: 'Santiago', points: 0, previousTime: '23:45' },
+  { id:'9', nick: 'Victor', points: 0, previousTime: '21:31' },
+  { id:'10', nick: 'Camo', points: 0, previousTime: '21:24' },
+  { id:'11', nick: 'Diego', points: 0, previousTime: '21:19' },
+  { id:'12', nick: 'Oscar', points: 0, previousTime: '21:02' },
+  { id:'13', nick: 'Fernando', points: 0, previousTime: '20:55' },
+  { id:'14', nick: 'Camilo', points: 0, previousTime: '20:43' },
+  { id:'15', nick: 'Chispas', points: 0, previousTime: '20:17' },
+  { id:'16', nick: 'Chiqui', points: 0, previousTime: '20:05' },
+  { id:'17', nick: 'Carlos', points: 0, previousTime: '19:54' },
+  { id:'18', nick: 'Cesar', points: 0, previousTime: '19:52' },
+  { id:'19', nick: 'Toño', points: 0, previousTime: '16:27' },
+  { id:'20', nick: 'Jonathan', points: 0, previousTime: '15:57' },
 ];
 
